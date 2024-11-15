08296125c0e5d714570bcb77133b8e2b0dea43c4
WinbertZhang <winbertzhang@gmail.com>
2024-11-15 13:14:07 -0800

    update resume

--- app/page.tsx
+++ app/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
             I&apos;m a Computer Science major studying @ UCSB
           </p>
           <button className="mt-8 px-6 py-2 border rounded-md border-blue text-blue hover:bg-blue hover:text-white transition-colors duration-300">
-            <Link href="/resume.pdf" target="_">
+            <Link href="/Winbert_Zhang_resume.pdf" target="_">
               RESUME
             </Link>
           </button>
